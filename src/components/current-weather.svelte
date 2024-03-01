a5de95728d5aeb1abecbbb4ffc28d3af14e2721a
Guilherme Campano <campano.guilherm@gmail.com>
2024-03-01 22:44:19 +0000

    Separate in components

---
+++ src/components/current-weather.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { latitude, longitude, forecast, isLoading } from '../stores/forecast.store';
+</script>
+
+<h4>Current Weather on {$latitude} | {$longitude}</h4>
+{#if $isLoading}
+  <p>Loading...</p>
+{/if}
+
+{#if $forecast}
+  <pre>
+    {@html JSON.stringify($forecast.current, null, 2)}
+  </pre>
+{/if}
+
+{#if $forecast === null}
+  <p>No data available</p>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,9 @@
 <script lang="ts">
-  let latitude = 40.7128;
-  let longitude = 74.0060;
-  let forecastPromise: Promise<any>;
-
-  $: {
-    forecastPromise = fetchForecast();
-  }
-
-  async function fetchForecast() {
-    console.log('fetching forecast');
-    const res = await fetch(`/forecast?latitude=${latitude.toFixed(4)}&longitude=${longitude.toFixed(4)}`);
-    const data = await res.json();
-    return data;
-  }
+	import CoordinatesForm from "../components/coordinates-form.svelte";
+  import CurrentWeather from "../components/current-weather.svelte";
 </script>
 
 <h1>Home page</h1>
 
-<input type="number" bind:value={latitude} />
-<input type="number" bind:value={longitude} />
-<button on:click={() => { forecastPromise = fetchForecast(); }}>Fetch Forecast</button>
-
-{#await forecastPromise then response}
-  <h4>Current Weather on {response.latitude} | {response.longitude}</h4>
-  <pre>
-    {@html JSON.stringify(response.current, null, 2)}
-</pre>
-{:catch error}
-  <p>Error: {error.message}</p>
-{/await}
+<CoordinatesForm />
+<CurrentWeather />
