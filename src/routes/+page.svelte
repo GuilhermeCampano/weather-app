49f2eff28ba6072797b338a956dc32750459ae39
Guilherme Campano <campano.guilherm@gmail.com>
2024-03-02 15:19:17 +0000

    Rename components to PascalCase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import CitySelect from "../components/city-select.svelte";
-	import CoordinatesForm from "../components/coordinates-form.svelte";
-  import CurrentWeather from "../components/current-weather.svelte";
+	import CitySelect from "../components/CitySelect.svelte";
+	import CoordinatesForm from "../components/CoordinatesForm.svelte";
+	import CurrentWeather from "../components/CurrentWeather.svelte";
 </script>
 
 <h1>Home page</h1>
