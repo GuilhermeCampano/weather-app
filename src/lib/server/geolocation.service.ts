cf803c9915ebf3e09f1be08b76f8511d6508dd07
Guilherme Campano <campano.guilherm@gmail.com>
2024-03-15 00:17:26 +0000

    Add environment variable

--- src/lib/server/geolocation.service.ts
+++ src/lib/server/geolocation.service.ts
@@ -1,10 +1,11 @@
+import { env } from '$env/dynamic/private';
 export class GeolocationService {
   readonly #API_KEY = 'AIzaSyC2YwoIGga-xHDKayrLaUSofcly_4bgRpk';
   readonly #AUTOCOMPLETE_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/autocomplete/json';
   readonly #PLACES_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/details/json';
 
   public getPlaceAutocomplete(input: string): Promise<google.maps.places.AutocompletePrediction[]> {
-    return fetch(`${this.#AUTOCOMPLETE_ENDPOINT}?input=${encodeURIComponent(input)}&key=${this.#API_KEY}`)
+    return fetch(`${this.#AUTOCOMPLETE_ENDPOINT}?input=${encodeURIComponent(input)}&key=${env.GOOGLE_API_KEY}`)
       .then(response => response.json())
       .then(data => data.predictions || [])
       .catch(error => {
@@ -14,7 +15,7 @@ export class GeolocationService {
   }
 
   public getPlaceDetails(placeId: string): Promise<google.maps.places.PlaceResult> {
-    return fetch(`${this.#PLACES_ENDPOINT}?place_id=${placeId}&key=${this.#API_KEY}`)
+    return fetch(`${this.#PLACES_ENDPOINT}?place_id=${placeId}&key=${env.GOOGLE_API_KEY}`)
       .then(response => response.json())
       .then(data => data.result)
       .catch(error => {
