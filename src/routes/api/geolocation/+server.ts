9a57d66ae751fba5557576837b8967e7f9c56ea2
Guilherme Campano <campano.guilherm@gmail.com>
2024-03-15 22:20:53 +0000

    Improve error handling

--- src/routes/api/geolocation/+server.ts
+++ src/routes/api/geolocation/+server.ts
@@ -1,20 +1,29 @@
 import { GeolocationService } from "$lib/server/geolocation-api.service";
 
 export async function GET({ url }: { url: URL }) {
-  console.log('GET /api/geolocation');
+  const placeId = <string>url.searchParams.get('placeId');
+  if (!placeId) {
+    console.error('Error: placeId is required');
+    return new Response('Error: placeId is required', {
+      headers: { 'content-type': 'application/json' },
+      status: 400
+    });
+  }
+
+  console.log(`GET /api/geolocation for placeId: ${placeId}`);
   try {
-    const placeId = <string>url.searchParams.get('placeId');
     const geolocationService = new GeolocationService();
     const response = await geolocationService.getPlaceDetails(placeId);
-    console.log('response', response);
+    console.log('Response from Geolocation API:', JSON.stringify(response, null, 2));
     return new Response(JSON.stringify(response), {
       headers: { 'content-type': 'application/json' },
       status: 200
     });
   }
   catch (error) {
-    console.error(error);
+    console.error('Error fetching geolocation data:', error);
     return new Response('Error fetching geolocation data', {
+      headers: { 'content-type': 'application/json' },
       status: 500
     });
   }
