87baae14c95b7005b81381cc7f1a1876fc0549a7
Guilherme Campano <campano.guilherm@gmail.com>
2024-03-16 15:31:33 +0000

    Validate Referer

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,13 +3,15 @@ import { TokenService } from '$lib/server/token.service';
 export async function handle({ event, resolve }) {
   const { request } = event;
 
-  if (!isApiRequest(request)) return resolve(event);
+  if (!isApiRequest(request)) { return resolve(event); }
 
   const token = extractToken(request);
 
-  if (!token) return new Response('Unauthorized: Missing token', { status: 401 });
+  if (!token) { return new Response('Unauthorized: Missing token', { status: 401 }); }
 
-  if (!TokenService.verifySignature(token)) return new Response('Unauthorized: Invalid token', { status: 403 });
+  if (!TokenService.verifySignature(token)) { return new Response('Unauthorized: Invalid token', { status: 403 }); }
+
+  if (!validReferer(request)) { return new Response('Unauthorized: Invalid origin', { status: 403 }); }
 
   return resolve(event);
 }
@@ -21,4 +23,13 @@ function isApiRequest(request: Request): boolean {
 function extractToken(request: Request): string {
   const authHeader = request.headers.get('Authorization');
   return authHeader?.split('Bearer ')[1] || '';
+}
+
+function validReferer(request: Request): boolean {
+  let referer = request.headers.get('Referer');
+  const serverOrigin = new URL(request.url).origin;
+  if (referer?.endsWith('/')) {
+    referer = referer.slice(0, -1);
+  }
+  return referer === serverOrigin;
 }
