785b492b4ddd9d6df102de79b4bdff5bb63c1a2d
Guilherme Campano <campano.guilherm@gmail.com>
2024-03-27 13:03:51 +0000

    Refactor forecast page

--- src/lib/components/Forecast/Forecast.svelte
+++ src/lib/components/Forecast/Forecast.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
-	import {
-		currentWeather,
-		hourlyForecastCards,
-		isLoading,
-		weekForecastCards
-	} from '$lib/stores/forecast.store';
-	import { isDesktop } from '$lib/stores/screen-detection.store';
 	import Card from '../UI/Card.svelte';
 	import SkeletonAnimation from '../UI/SkeletonAnimation.svelte';
+	import { latitude, longitude } from '$lib/stores/search-location.store';
+	import { isDesktop } from '$lib/stores/screen-detection.store';
 	import CurrentWeather from './CurrentWeather.svelte';
 	import ForecastHourly from './ForecastHourly.svelte';
 	import ForecastNextDays from './ForecastNextDays.svelte';
+	import ApiService from '$lib/utils/api-service';
 </script>
 
-{#if $isLoading || $currentWeather === null}
+{#await ApiService.getForecast($latitude, $longitude)}
 	<h6><SkeletonAnimation width="200px" height="2rem" /></h6>
 	<Card size="large" isLoading={true} />
 
@@ -26,28 +22,28 @@
 			<Card size="small" isLoading={true} />
 		{/each}
 	</div>
-{:else}
+{:then { current, daily, hourly }}
 	<h6 class="forecast__title">Today</h6>
 
 	{#if $isDesktop}
 		<Card size="large">
-			<CurrentWeather forecastCurrentCard={$currentWeather} />
-			<ForecastHourly hourlyForecastCards={$hourlyForecastCards} />
+			<CurrentWeather forecastCurrentCard={current} />
+			<ForecastHourly hourlyForecastCards={hourly} />
 		</Card>
 	{:else}
 		<Card size="large">
-			<CurrentWeather forecastCurrentCard={$currentWeather} />
+			<CurrentWeather forecastCurrentCard={current} />
 		</Card>
 		<h6>Hourly Forecast</h6>
 		<Card size="large" isCompact>
-			<ForecastHourly hourlyForecastCards={$hourlyForecastCards} />
+			<ForecastHourly hourlyForecastCards={hourly} />
 		</Card>
 	{/if}
-
 	<h6>Next 7 Days</h6>
-
-	<ForecastNextDays weekForecastCards={$weekForecastCards} />
-{/if}
+	<ForecastNextDays weekForecastCards={daily} />
+{:catch error}
+	<p>{error.message}</p>
+{/await}
 
 <style>
 	.forecast__title {
@@ -59,6 +55,7 @@
 			margin-top: var(--spacing-sm);
 		}
 	}
+
 	.forecast__loading-week {
 		display: flex;
 		flex-direction: row;
