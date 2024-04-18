24779e655e4753165e1f5993ffc311921f670185
Guilherme Campano <campano.guilherm@gmail.com>
2024-04-18 23:02:34 +0100

    Validate token

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,21 +6,28 @@ import { Endpoints } from "$lib/models";
 import { verifyToken } from "$lib/server/token.service";
 
 
-export async function handleFetch(request: Request): Promise<Response> {
-  if (!validReferer(request)) { return new Response('Unauthorized: Invalid origin', { status: 403 }); }
-
-  const token = extractToken(request);
-
-  if (!token) { return new Response('Unauthorized: Missing token', { status: 401 }); }
-  const isTokenValid = await verifyToken(token);
-  if (!isTokenValid) { return new Response('Unauthorized: Invalid token', { status: 403 }); }
-
-  if (PRIVATE_MOCK_API === 'TRUE') {
-    console.log('Mocking API response');
-    return mockAPIResponse(request);
+export async function handle({ event, resolve }): Promise<Response> {
+  if (!event.request.url.includes('/api/')) {
+    return await resolve(event);
   }
 
-  return fetch(request);
+  const token = extractToken(event.request);
+
+  if (!token) { 
+    return new Response('Unauthorized: Missing token', { status: 401 }); 
+  }
+  const isTokenValid = await verifyToken(token);
+  if (!isTokenValid) { 
+    return new Response('Unauthorized: Invalid token', { status: 403 }); 
+  }
+
+
+  if (PRIVATE_MOCK_API.toLocaleUpperCase() === 'TRUE') {
+    console.log('Mocking API response');
+    return mockAPIResponse(event.request);
+  }
+
+  return await resolve(event);
 }
 
 function extractToken(request: Request): string {
@@ -28,24 +35,24 @@ function extractToken(request: Request): string {
   return authHeader?.split('Bearer ')[1] || '';
 }
 
-function validReferer(request: Request): boolean {
-  let referer = request.headers.get('Referer');
-  const serverOrigin = new URL(request.url).origin;
-  if (referer?.endsWith('/')) {
-    referer = referer.slice(0, -1);
-  }
-  return referer === serverOrigin;
-}
+// function validReferer(request: Request): boolean {
+//   let referer = request.headers.get('Referer');
+//   const serverOrigin = new URL(request.url).origin;
+//   if (referer?.endsWith('/')) {
+//     referer = referer.slice(0, -1);
+//   }
+//   return referer === serverOrigin;
+// }
 
 function mockAPIResponse(request: Request): Response {
   const status200 = { status: 200 };
   if (request.url.includes(Endpoints.FORECAST)) {
     return new Response(JSON.stringify(forecastMockResponse), status200);
-  }  
+  }
   if (request.url.includes((Endpoints.AUTOCOMPLETE))) {
     return new Response(JSON.stringify(autocompleteMockResponse), status200);
   }
-  if(request.url.includes((Endpoints.GEOLOCATION))) {
+  if (request.url.includes((Endpoints.GEOLOCATION))) {
     return new Response(JSON.stringify(geolocationDetailsMockResponse), status200);
   }
   return new Response('Not found', { status: 404 });
